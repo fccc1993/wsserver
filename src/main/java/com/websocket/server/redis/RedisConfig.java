88659f3784f67c8e71d1f5beeea222c60b4b202c
fccc1993 <954766551@qq.com>
2018-01-03 14:19:43 +0800

    websocket

--- src/main/java/com/websocket/server/redis/RedisConfig.java
+++ src/main/java/com/websocket/server/redis/RedisConfig.java
@@ -1,84 +1,84 @@
 package com.websocket.server.redis;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cache.CacheManager;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-import org.springframework.data.redis.cache.RedisCacheManager;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
-import org.springframework.data.redis.serializer.RedisSerializer;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
-import redis.clients.jedis.JedisPoolConfig;
-
-/**
- * @author halochen
- * @since 2017-10-23
- */
-@Configuration
-@PropertySource("classpath:config/redis.properties")
-public class RedisConfig {
-    @Value("${redis.host}")
-    private String host;
-    @Value("${redis.port}")
-    private int port;
-    @Value("${redis.password}")
-    private String password;
-    @Value("${redis.pool.max-total}")
-    private int maxTotal;
-    @Value("${redis.pool.max-idle}")
-    private int maxIdle;
-    @Value("${redis.pool.min-idle}")
-    private int minIdle;
-    @Value("${redis.pool.max-wait}")
-    private int maxWait;
-
-    //@PropertySource要配合PropertySourcesPlaceholderConfigurer才能使用@Value注解.
-    @Bean
-    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
-        return new PropertySourcesPlaceholderConfigurer();
-    }
-
-    @Bean
-    public JedisConnectionFactory jedisConnectionFactory() {
-        JedisConnectionFactory factory = new JedisConnectionFactory();
-//        factory.setPassword(password);
-        factory.setHostName(host);
-        factory.setPort(port);
-        JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
-        jedisPoolConfig.setMaxTotal(maxTotal);
-        jedisPoolConfig.setMaxIdle(maxIdle);
-        jedisPoolConfig.setMinIdle(minIdle);
-        jedisPoolConfig.setMaxWaitMillis(maxWait);
-        factory.setPoolConfig(jedisPoolConfig);
-        return factory;
-    }
-
-    @Bean
-    public CacheManager cacheManager(RedisTemplate<?,?> redisTemplate) {
-        CacheManager cacheManager = new RedisCacheManager(redisTemplate);
-        return cacheManager;
-
-    }
-
-    @Bean
-    public RedisTemplate<String, Object> getRedisTemplate(RedisConnectionFactory factory) {
-        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<String, Object>();
-        redisTemplate.setConnectionFactory(factory);
-        //key序列化方式,但是如果方法上有Long等非String类型的话，会报类型转换错误
-        //所以在没有自己定义key生成策略的时候，以下这个代码建议不要这么写，可以不配置或者自己实现ObjectRedisSerializer
-        RedisSerializer<String> redisSerializer = new StringRedisSerializer();//Long类型不可以会出现异常信息;
-        redisTemplate.setKeySerializer(redisSerializer);
-        redisTemplate.setHashKeySerializer(redisSerializer);
-        redisTemplate.setValueSerializer(redisSerializer);
-        redisTemplate.setHashValueSerializer(redisSerializer);
-        redisTemplate.afterPropertiesSet();
-
-        return redisTemplate;
-    }
-
-}
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.cache.CacheManager;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.PropertySource;
+//import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+//import org.springframework.data.redis.cache.RedisCacheManager;
+//import org.springframework.data.redis.connection.RedisConnectionFactory;
+//import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+//import org.springframework.data.redis.core.RedisTemplate;
+//import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
+//import org.springframework.data.redis.serializer.RedisSerializer;
+//import org.springframework.data.redis.serializer.StringRedisSerializer;
+//import redis.clients.jedis.JedisPoolConfig;
+//
+///**
+// * @author halochen
+// * @since 2017-10-23
+// */
+//@Configuration
+//@PropertySource("classpath:config/redis.properties")
+//public class RedisConfig {
+//    @Value("${redis.host}")
+//    private String host;
+//    @Value("${redis.port}")
+//    private int port;
+//    @Value("${redis.password}")
+//    private String password;
+//    @Value("${redis.pool.max-total}")
+//    private int maxTotal;
+//    @Value("${redis.pool.max-idle}")
+//    private int maxIdle;
+//    @Value("${redis.pool.min-idle}")
+//    private int minIdle;
+//    @Value("${redis.pool.max-wait}")
+//    private int maxWait;
+//
+//    //@PropertySource要配合PropertySourcesPlaceholderConfigurer才能使用@Value注解.
+//    @Bean
+//    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+//        return new PropertySourcesPlaceholderConfigurer();
+//    }
+//
+//    @Bean
+//    public JedisConnectionFactory jedisConnectionFactory() {
+//        JedisConnectionFactory factory = new JedisConnectionFactory();
+////        factory.setPassword(password);
+//        factory.setHostName(host);
+//        factory.setPort(port);
+//        JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
+//        jedisPoolConfig.setMaxTotal(maxTotal);
+//        jedisPoolConfig.setMaxIdle(maxIdle);
+//        jedisPoolConfig.setMinIdle(minIdle);
+//        jedisPoolConfig.setMaxWaitMillis(maxWait);
+//        factory.setPoolConfig(jedisPoolConfig);
+//        return factory;
+//    }
+//
+//    @Bean
+//    public CacheManager cacheManager(RedisTemplate<?,?> redisTemplate) {
+//        CacheManager cacheManager = new RedisCacheManager(redisTemplate);
+//        return cacheManager;
+//
+//    }
+//
+//    @Bean
+//    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
+//        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<String, Object>();
+//        redisTemplate.setConnectionFactory(factory);
+//        //key序列化方式,但是如果方法上有Long等非String类型的话，会报类型转换错误
+//        //所以在没有自己定义key生成策略的时候，以下这个代码建议不要这么写，可以不配置或者自己实现ObjectRedisSerializer
+//        RedisSerializer<String> redisSerializer = new StringRedisSerializer();//Long类型不可以会出现异常信息;
+//        redisTemplate.setKeySerializer(redisSerializer);
+//        redisTemplate.setHashKeySerializer(redisSerializer);
+//        redisTemplate.setValueSerializer(redisSerializer);
+//        redisTemplate.setHashValueSerializer(redisSerializer);
+//        redisTemplate.afterPropertiesSet();
+//
+//        return redisTemplate;
+//    }
+//
+//}
